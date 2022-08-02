4c9c0f7a9f2f2439c5858072502337cb612b11f3
Grzegorz Migal <blofeld@poczta.fm>
2022-08-02 17:33:03 +0200

    Adding some songs to the new created playlist

--- src/test/java/PlaylistTest.java
+++ src/test/java/PlaylistTest.java
@@ -5,9 +5,11 @@ import io.qameta.allure.Feature;
 import io.qameta.allure.Step;
 import io.restassured.response.Response;
 import model.playlist.Playlist;
+import org.apache.http.HttpStatus;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.post;
+import static java.lang.System.getProperty;
 import static org.assertj.core.api.Assertions.assertThat;
 import static utils.FakerGenerator.getPlaylistDescription;
 import static utils.FakerGenerator.getPlaylistName;
@@ -29,19 +31,22 @@ public class PlaylistTest extends BaseTest{
     }
 
     @Test(priority = 1)
-    public void createPlaylistTest(){
+    public void createPlaylistTest() throws InterruptedException {
         Playlist playlist = playlistBuilder(getPlaylistName(),getPlaylistDescription(),true);
         Response response = PlaylistAPI.post(playlist);
         Playlist playResp = response.as(Playlist.class);
         DataStore.PLAYLIST_ID = response.path("id");
+        Thread.sleep(3000);
         assertPlaylistEqual(playResp,playlist);
         System.out.println(DataStore.PLAYLIST_ID);
     }
 
     @Test(priority = 2)
     public void addSongsTest(){
-
-
+        Response response = PlaylistAPI.addSong(DataStore.PLAYLIST_ID,getProperty("blur"),0);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_CREATED);
+        PlaylistAPI.addSong(DataStore.PLAYLIST_ID, getProperty("immortality"),1);
+        PlaylistAPI.addSong(DataStore.PLAYLIST_ID, getProperty("oczy"),2);
     }
 
 
